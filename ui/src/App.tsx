a4c795f405e07e89e85ce6e92aceded228502857
Wojciech Jedras <jedras.wojciech@gmail.com>
2022-03-30 00:55:32 +0200

    ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import { GetPantry } from '../src/api/Api'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { MenuItem, Select, TextField } from '@mui/material';
+
+
+function App() {
+  return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="pantry" element={<Pantry />} />
+        </Routes>
+      </div>
+    );
+}
+
+function filterRows(rows: object[], nameFilter: string, categoryFilter: string) {
+  return rows
+}
+
+function Pantry() {
+  const [category, setCategory] = useState("all")
+  const [name, setName] = useState("")
+  const [rows, setRows] = useState(GetPantry())
+
+  const displayRows = filterRows(rows, name, category)
+  const columns: GridColDef[] = [
+    { field: 'name' },
+    { field: 'number' },
+    { field: 'exp_date' },
+    { field: "scale" },
+    { field: 'note' },
+  ];
+  return <div style={{ height: 500, width: '50%' }}>
+    <TextField 
+      id="outlined-basic"
+      variant="outlined" 
+      onChange={e => {
+        if(e.target.value.length >= 3){
+          setName(e.target.value)
+        } else {
+          setName("")
+        }
+      }}  
+    />
+    <Select
+      value={category}
+      label="Age"
+      onChange={e => setCategory(e.target.value)}>
+        <MenuItem value="all">Wszystkie</MenuItem>
+        <MenuItem value="food">Jedzenie</MenuItem>
+        <MenuItem value="cleaning">Środki czystości</MenuItem>
+    </Select>
+    <DataGrid
+        rows={rows.filter(row => row.name.includes(name) && category == "all" || row.category === category)}
+        columns={columns}
+      />
+  </div>
+}
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="images/PusheenFridge.PNG" className="App-logo" alt="logo" />
+        <p>
+          Czy w lodówce jest jedzenie?
+        </p>
+        <nav>
+        <Link to="/pantry">Sprawdź</Link>
+      </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/src/api/Api.tsx
@@ -0,0 +1,8 @@
+export function GetPantry() {
+    const pantry = [{id: 1, name: "masło", number: "1", scale: "count", exp_date: "12-01-2023", note: "zamrazarka", category: "food"},
+    {id: 2, name: "laskolada", number: "1", scale: "count", exp_date: "12-01-2023", note: "zamrazarka", category: "food"},
+    {id: 3, name: "pistacje", number: "1", scale: "count", exp_date: "12-01-2023", note: "zamrazarka", category: "food"},
+    {id: 4, name: "mydło", number: "1", scale: "count", exp_date: "12-01-2023", note: "zamrazarka", category: "cleaning"},
+    {id: 5, name: "proszek", number: "1", scale: "kg", exp_date: "12-01-2023", note: "zamrazarka", category: "cleaning"}]
+    return pantry
+}
